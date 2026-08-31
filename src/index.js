06d3561e2559aa3fc66bac4a27e883108a41d940
Rossy321 <140844023+Rossy321@users.noreply.github.com>
2026-08-31 12:06:04 +0000

    create menu module
    Include event listenr button to enter menu tab.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import loadHomeContent from "./page-load.js";
-import contact from "./contact.js"
+import contact from "./contact.js";
+import menu from "./menu.js";
+import "./styles.css";
 
 loadHomeContent();
 
@@ -14,5 +16,14 @@ const homeButton = document.querySelector(".home");
 homeButton.addEventListener("click", () => {
     loadHomeContent();
 });
+
+const menuButton = document.querySelector(".menu");
+
+menuButton.addEventListener("click", () => {
+    menu();
+});
+
+
+
 
 
---
+++ src/menu.js
@@ -0,0 +1,107 @@
+import chickenSrc from "./chicken-taco.png"  
+import beefSrc from "./beef-taco.png"
+import fishSrc from "./fish-taco.png"
+export default function menu() {
+    
+    const menu = document.getElementById("content");
+
+    menu.innerHTML = "";
+
+    const header = document.createElement("h1");
+
+    header.textContent = "Menu";
+
+    menu.appendChild(header);
+
+    const menuItems = document.createElement("div");
+
+    menuItems.classList.add("menuItems");
+
+    menu.appendChild(menuItems);
+
+    
+    const chickenTaco = document.createElement("div");
+    chickenTaco.classList.add("chicken-taco");
+
+    const chickenImg = document.createElement("img");
+    chickenImg.src =  chickenSrc;
+    chickenImg.alt = "Chicken Taco";
+    chickenImg.classList.add("menuItem");
+
+
+    const chickenTacoHed = document.createElement("h3");
+    chickenTacoHed.textContent = ("Chicken Taco");
+
+    const chickenTacoPrice = document.createElement("p");
+    chickenTacoPrice.textContent = "£5.00";
+
+    chickenTaco.append(chickenImg, chickenTacoHed, chickenTacoPrice);
+
+    menuItems.appendChild(chickenTaco);
+
+
+     const beefTaco = document.createElement("div");
+     beefTaco.classList.add("beef-taco");
+
+    const beefImg = document.createElement("img");
+    beefImg.src =  beefSrc;
+    beefImg.alt = "Beef Taco";
+    beefImg.classList.add("menuItem");
+
+
+    const beefTacoHed = document.createElement("h3");
+    beefTacoHed.textContent = ("Beef Taco");
+
+    const beefTacoPrice = document.createElement("p");
+    beefTacoPrice.textContent = "£5.00";
+
+    beefTaco.append(beefImg, beefTacoHed, beefTacoPrice);
+
+    menuItems.appendChild(beefTaco);
+
+
+    const fishTaco = document.createElement("div");
+     fishTaco.classList.add("fish-taco");
+
+    const fishImg = document.createElement("img");
+    fishImg.src =  fishSrc;
+    fishImg.alt = "Fish Taco";
+    fishImg.classList.add("menuItem");
+
+
+    const fishTacoHed = document.createElement("h3");
+    fishTacoHed.textContent = ("Fish Taco");
+
+    const fishTacoPrice = document.createElement("p");
+    fishTacoPrice.textContent = "£5.00";
+
+    fishTaco.append(fishImg, fishTacoHed, fishTacoPrice);
+
+    menuItems.appendChild(fishTaco);
+
+
+    
+    const veganTaco = document.createElement("div");
+     veganTaco.classList.add("fish-taco");
+
+    const veganImg = document.createElement("img");
+    veganImg.src =  fishSrc;
+    veganImg.alt = "Fish Taco";
+    veganImg.classList.add("menuItem");
+
+
+    const veganTacoHed = document.createElement("h3");
+    veganTacoHed.textContent = ("Vegan Taco");
+
+    const veganTacoPrice = document.createElement("p");
+    veganTacoPrice.textContent = "£5.00";
+
+    veganTaco.append(veganImg, veganTacoHed, veganTacoPrice);
+
+    menuItems.appendChild(veganTaco);
+
+
+
+
+
+}
